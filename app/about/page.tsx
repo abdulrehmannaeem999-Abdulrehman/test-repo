385aec0a039c2deba61e4b66d759edad6ba8d9ec
Abdulrehman sheikhani <abdulrehman914@gmail.com>
2024-10-27 19:05:28 +0500

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import next from "next";
+
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      nice work
+    </div>
+  )
+}
+
+export default about
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Helloworld</h1>
+      <button color="blue">
+        <h1><a href="/about">Abdulrehman</a></h1></button>
+      
     </div>
   );
 }
